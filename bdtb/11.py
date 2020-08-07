d41b15e7fd48871c8c0a5736c3f501b054e0c895
ldx-web <ldx13012101788@163.com>
2020-08-07 12:57:22 +0800

    爬取百度贴吧--python吧文件

---
+++ bdtb/11.py
@@ -0,0 +1,3 @@
+
+a= {"id":5545672434,"author_name":"\u53f2\u83b1\u51b02\u53f7","author_nickname":'null',"author_portrait":"tb.1.110231ae.E8_VEbu8R3Ge46y4bKJ1CQ","first_post_id":117915094500,"reply_num":25,"is_bakan":'null',"vid":"","is_good":'',"is_top":'',"is_protal":'',"is_membertop":'',"is_multi_forum":'',"frs_tpoint":''}
+print(a["id"])
